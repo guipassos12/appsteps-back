ddf6ccffda2cc5acbcd1068b1322cf9166aeab4c
Guilherme Passos <guipassos7@gmail.com>
2019-03-12 11:47:17 -0300

    add certo para heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const db = require('./db/db');
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
